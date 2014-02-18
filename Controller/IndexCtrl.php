741f1662ae3aecfc48bcf81f9c6606216599b6b9
seiferxx <seifer.ch@gmail.com>
2014-02-18 11:51:28 -0600

    Created indexCtrl and view index

---
+++ Controller/IndexCtrl.php
@@ -0,0 +1,21 @@
+<?php
+	class IndexCtrl {
+		
+		public function run( $dbCon ){
+			switch ( $_GET[ "action" ]){
+				case "index":
+					$header = file_get_contents( "./View/Header.html" );
+					$content = file_get_contents( "./View/MainPage.html" );
+					$footer = file_get_contents( "./View/Footer.html" );
+						
+					echo $header;
+					echo $content;
+					echo $footer;
+					break;
+					default:
+						//Send to 404 Error
+			} 
+		}
+	}
+
+?>
