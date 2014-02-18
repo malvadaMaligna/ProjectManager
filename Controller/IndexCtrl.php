6a00256fd586121d1e80a87853e1e75f57e982e4
dis <dis@dis-PC>
2014-02-18 15:29:29 -0600

    added session managment

--- Controller/IndexCtrl.php
+++ Controller/IndexCtrl.php
@@ -2,18 +2,40 @@
 	class IndexCtrl {
 		
 		public function run( $dbCon ){
+			session_start();
 			switch ( $_GET[ "action" ]){
 				case "index":
 					$header = file_get_contents( "./View/Header.html" );
 					$content = file_get_contents( "./View/MainPage.html" );
 					$footer = file_get_contents( "./View/Footer.html" );
-						
+					
+					if( !isset( $_SESSION[ 'user' ] ) ){
+						$navbarContent = "You are not logged in";
+						$navbarOpt = "<li><a href=\"./index.php?control=login&action=signIn\">Sign In</a></li>";
+						$header = str_replace( "{user}", $navbarContent , $header );
+						$header = str_replace( "{userOpt}", $navbarOpt , $header );
+					}
+					else{
+						$navbarContent = $_SESSION[ "user" ];
+						$navbarOpt = "<li class=\"dropdown\">
+											<a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\"><i class=\"glyphicon glyphicon-home\"></i> Home<b class=\"caret\"></b></a>
+											<ul class=\"dropdown-menu\">
+												<li><a href=\"#\"><i class=\"glyphicon glyphicon-user\"></i> Profile</a></li>
+												<li><a href=\"#\"><i class=\"glyphicon glyphicon-book\"></i> Projects</a></li>
+												<li><a href=\"#\"><i class=\"glyphicon glyphicon-wrench\"></i> Settings</a></li>
+												<li class=\"divider\"></li>
+												<li><a href=\"./index.php?control=login&action=logOut\"><i class=\"glyphicon glyphicon-log-out\" ></i> Log out</a></li>
+											</ul>
+										</li>";
+						$header = str_replace( "{user}", $navbarContent , $header );
+						$header = str_replace( "{userOpt}", $navbarOpt , $header );
+					}
 					echo $header;
 					echo $content;
 					echo $footer;
 					break;
-					default:
-						//Send to 404 Error
+				default:
+					//Send to 404 Error
 			} 
 		}
 	}
--- Controller/LoginCtrl.php
+++ Controller/LoginCtrl.php
@@ -3,25 +3,55 @@
 	class LoginCtrl {
 		
 		public function run( $dbCon ){
+			session_start();
 			switch ( $_GET[ "action" ] ){
 				case "signIn":
-					$header = file_get_contents( "./View/Header.html" );
-					$content = file_get_contents( "./View/Login.html" );
-					$footer = file_get_contents( "./View/Footer.html" );
-					
-					echo $header;
-					echo $content;
-					echo $footer;
+					if( !isset( $_SESSION[ 'user' ] ) ){
+						$header = file_get_contents( "./View/Header.html" );
+						$content = file_get_contents( "./View/Login.html" );
+						$footer = file_get_contents( "./View/Footer.html" );
+						
+						$navbarContent = "You are not logged in";
+						$navbarOpt = "<li><a href=\"./index.php?control=login&action=signIn\">Sign In</a></li>";
+						$header = str_replace( "{user}", $navbarContent , $header );
+						$header = str_replace( "{userOpt}", $navbarOpt , $header );
+						echo $header;
+						echo $content;
+						echo $footer;
+					}
+					else{
+						header( "Location: ./index.php?control=index&action=index" );
+					}
 					break;
 				case "authenticate":
-					//TODO validate input values
-					require_once './Model/LoginMdl.php';
-					$login = new LoginMdl( $dbCon );
-					$user = $_POST[ "user" ];
-					$passwd = sha1( $_POST[ "passwd" ] );
-					$result = $login -> authenticate( $user );
-					$row = $result -> fetch_row( );
-					var_dump( $result );
+					if( !isset( $_SESSION[ 'user' ] ) ){
+						//TODO validate input values
+						require_once './Model/LoginMdl.php';
+						$login = new LoginMdl( $dbCon );
+						$user = $_POST[ "user" ];
+						$passwd = sha1( $_POST[ "passwd" ] );
+						$result = $login -> authenticate( $user );
+						$row = $result -> fetch_row( );
+						$key = $row[ 0 ];
+						if( $key == $passwd ){
+							$_SESSION[ "user" ] = $user;
+						}
+						header( "Location: ./index.php?control=index&action=index" );
+					}
+					else{
+						//Send to error
+					}
+					break;
+				case "logOut":
+					if( isset( $_SESSION[ 'user' ] ) ){
+						session_destroy( );
+						session_unset( );
+						setcookie( session_name( ), ' ', time( ) - 3600 );
+						header( "Location: ./index.php?control=index&action=index" );
+					}
+					else{
+						//Send to error
+					}
 					break;
 				default: 
 					//Send to 404 Error
