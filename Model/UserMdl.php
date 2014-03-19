7516f937705840a32b1e85d96e9e0d53784b9843
jean.casillas <jean.casillas@jpcasillas>
2014-03-19 11:57:09 -0600

    Added querys for insert user

--- Model/UserMdl.php
+++ Model/UserMdl.php
@@ -8,6 +8,17 @@
 		}
 		
 		//TODO funciones query
+		public function setUser(){
+			$query = "INSERT INTO user ( name, lastname, nickName, password, email, userType, code, regDate ) VALUES ( \"".$date."\", ) ";
+			$result = $this -> connection -> query( $query ) or die( "DB Error: BlogMdl.setEntryBlog: Error " );
+			return $result;
+		}
+		
+		public function setUserContact(){
+			$query = "INSERT INTO usercontact ( cellphone, facebook, github, google+, twitter, idUser ) VALUES ( ) ";
+			$result = $this -> connection -> query( $query ) or die( "DB Error: BlogMdl.setEntryBlog: Error " );
+			return $result;
+		}
 	}
 
 ?>
